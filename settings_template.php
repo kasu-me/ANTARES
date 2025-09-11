12026c7367d5573104ab97fb3f1644e54c5943cf
ahozura <66002768+kasu-me@users.noreply.github.com>
2025-09-11 16:58:28 +0900

    設定変数のグループを変更

--- settings_template.php
+++ settings_template.php
@@ -16,12 +16,12 @@ $SIMUTRANS_PAKSET="pak128japan";
 $SIMUTRANS_ADMIN_PASSWORD="examplepassword";
 $SIMUTRANS_LANG="ja";
 $SIMUTRANS_IP="192.0.2.1:".$SIMUTRANS_SERVER_PORT;
+$SIMUTRANS_LOG_PATH=$SIMUTRANS_DIR."/simutrans.log";//サーバのログ出力先
 
 //pakset更新関連
 $SIMUTRANS_PAK_VERSION_PATH="https://example.com/simutrans/updates.txt";//paksetのバージョンを記載したテキストファイルのURL
 $SIMUTRANS_UNDER_MAINTENANCE_URL="https://example.com/simutrans/under_maintenance";//このURLが404以外を返す場合はメンテナンス中とみなす
 $PAK_LINK="https://example.com/simutrans/paksets.zip";//paksetのダウンロードリンク
-$SIMUTRANS_LOG_PATH=$SIMUTRANS_DIR."/simutrans.log";//サーバのログ出力先
 
 //pak追加申請関連
 $TEMPORARY_PAK_FILE_DIRECTORY_PATH="/home/example/pak/files";//追加申請されたpakを一時的に置いておくディレクトリのパス
@@ -33,9 +33,9 @@ $DISCORD_OAUTH2_CLIENT_SECRET="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 $DISCORD_GUILD_ID="999999999999999999";//このIDのサーバに参加しているユーザのみログインを許可
 $DISCORD_GUILD_NAME="servername";//サーバ名。任意の文字列
 $SESSION_ID_DETERMINE_GUILD="isjoined".$DISCORD_GUILD_NAME;
-$ADMIN_DISCORD_USER_IDS=["<@999999999999999999>"];//管理者のDiscordアカウントID
 
-//Discord Webhook URL
+//Discord Webhook
+$ADMIN_DISCORD_USER_IDS=["<@999999999999999999>"];//管理者のDiscordアカウントID
 $DISCORD_WEBHOOK_URL_NOTICE_CHANNEL="https://discord.com/api/webhooks/9999999999999999999/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";//お知らせチャンネル投稿用
 $DISCORD_WEBHOOK_URL_ADMIN_CHANNEL="https://discord.com/api/webhooks/9999999999999999999/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";//管理者用チャンネル投稿用
 
