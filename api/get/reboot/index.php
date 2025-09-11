a1cf9999305282a347d0a9a2bed5fcff3915b29e
ahozura <66002768+kasu-me@users.noreply.github.com>
2025-09-11 11:19:30 +0900

    プロセス起動時、既にプロセスが立ち上がっていれば何もしないよう変更

--- api/get/reboot/index.php
+++ api/get/reboot/index.php
@@ -5,6 +5,16 @@ include($_SERVER["DOCUMENT_ROOT"]."/settings.php");
 include($_SERVER["DOCUMENT_ROOT"]."/auth/common.php");
 
 if(isLogIn() && $_SESSION[$SESSION_ID_DETERMINE_GUILD]){
+	//プロセスが起動している場合は何もしない
+	$process_check_command='ps aux | grep "'.$SIMUTRANS_BIN.' -server" | grep -v grep';
+	exec($process_check_command,$process_check_output);	
+	if(count($process_check_output)>0){
+		header('HTTP/1.0 409');
+		echo '{"message":"Simutransはすでに起動しています。"}';
+		exit();
+	}
+
+	//プロセスが起動していない場合は起動する
 	$nettool_command='export SIMUTRANS_DIR=\''.$SIMUTRANS_DIR.'\';nohup '.$SIMUTRANS_BIN.' -server '.$SIMUTRANS_SERVER_PORT.' -server_admin_pw "'.$SIMUTRANS_ADMIN_PASSWORD.'" -objects '.$SIMUTRANS_PAKSET.' -lang '.$SIMUTRANS_LANG.' > '.$SIMUTRANS_LOG_PATH.' &';
 	exec($nettool_command,$nettool_output);
 	echo "did";	
